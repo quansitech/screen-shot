f1b6632b076cd2f01f7804a63de3f1557458273c
tider <tider@quansitech.com>
2022-04-13 17:38:32 +0800

    t

--- package/index.js
+++ package/index.js
@@ -15,24 +15,36 @@ app.post('/', async function (req, res) {
         alioss_object_key = `screen-shot/${md5(JSON.stringify(req.body))}.png`;
     }
 
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    page.setViewport({
-        width: parseInt(width),
-        height: parseInt(height)
-    });
-    await page.goto(url);
-    const imgBuffer = await page.screenshot({fullPage: true });
-    const store = new OSS({
-        region: process.env.ALIOSS_REGION,
-        accessKeyId: process.env.ALIOSS_ACCESS_KEY_ID,
-        accessKeySecret: process.env.ALIOSS_ACCESS_KEY_SECRET,
-        bucket: process.env.ALIOSS_BUCKET
-     });
-    const ossRes = await store.put(alioss_object_key, imgBuffer);
-    await browser.close();
+    let ossRes;
+    try{
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        page.setViewport({
+            width: parseInt(width),
+            height: parseInt(height)
+        });
+        await page.goto(url);
+        const imgBuffer = await page.screenshot({fullPage: true });
+        const store = new OSS({
+            region: process.env.ALIOSS_REGION,
+            accessKeyId: process.env.ALIOSS_ACCESS_KEY_ID,
+            accessKeySecret: process.env.ALIOSS_ACCESS_KEY_SECRET,
+            bucket: process.env.ALIOSS_BUCKET
+         });
+        ossRes = await store.put(alioss_object_key, imgBuffer);
+        await browser.close();
+    }
+    catch(e){
+        console.log(e);
+        res.send({
+            code: -1,
+            msg: e.message
+        });
+        return;
+    }
+    
 
     res.send(ossRes)
 })
